dcabea12318ed0681a822f08b751dc43bdc1706d
jx <frosty6523@gmail.com>
2020-03-19 21:11:45 +0000

    Add files via upload

--- IMDb to NOW TV.py
+++ IMDb to NOW TV.py
@@ -17,10 +17,10 @@ password = config[3]
 options = Options()
 options.add_argument("--headless")
 
-# driver = webdriver.Chrome(executable_path="chromedriver.exe", chrome_options=options)
+driver = webdriver.Chrome(executable_path="chromedriver.exe", chrome_options=options)
 
 # Non-headless webdriver for debugging.
-driver = webdriver.Chrome(executable_path="chromedriver.exe")
+# driver = webdriver.Chrome(executable_path="chromedriver.exe")
 
 driver.get("https://www.imdb.com/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.imdb.com%2Fregistration%2Fap-signin-handler%2Fimdb_us&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=imdb_us&openid.mode=checkid_setup&siteState=eyJvcGVuaWQuYXNzb2NfaGFuZGxlIjoiaW1kYl91cyIsInJlZGlyZWN0VG8iOiJodHRwczovL3d3dy5pbWRiLmNvbS8_cmVmXz1sb2dpbiJ9&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&tag=imdbtag_reg-20")
 
